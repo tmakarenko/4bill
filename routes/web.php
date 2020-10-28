db61e5f71073dc8afda2fc465c9a1130d4942fe5
tmakarenko <trayzer94>
2020-10-28 18:29:29 +0200

    added middleware to check admin

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::prefix('/cabinet')->group(function () {
     Route::get('/', [App\Http\Controllers\CabinetController::class, 'index'])->name('cabinet');
-    Route::get('/list', [App\Http\Controllers\CabinetController::class, 'list'])->name('cabinetList');
+    Route::get('/list', [App\Http\Controllers\CabinetController::class, 'list'])->name('cabinetList')->middleware('is.admin');
 });
 
-Route::get('/user/{user}/deactivate', [App\Http\Controllers\UserController::class, 'deactivate'])->name('userDeactivate');
+Route::get('/user/{user}/deactivate', [App\Http\Controllers\UserController::class, 'deactivate'])->name('userDeactivate')->middleware('is.admin');
